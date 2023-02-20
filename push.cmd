4537be0313208dc2255a14e9f569e511fd94cfce
mitang01 <99809375+mitang01@users.noreply.github.com>
2023-02-20 18:11:12 +0100

    Update push.cmd

--- push.cmd
+++ push.cmd
@@ -3,3 +3,12 @@ copy preprocess.ipynb C:\Users\86130\TRFs-on-EEG
 cd C:\Users\86130\TRFs-on-EEG
 git add preprocess.ipynb 
 git push
+
+
+# -m flag provides a human friendly message describing the change
+git commit -m "Adds customer data analysis notebook"
+
+
+# Develop in a Branch (whose name is customer_data_insights)
+git checkout -b customer_data_insights
+git push --set-upstream origin customer_data_insights
